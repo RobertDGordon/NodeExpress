a4d640b68034ce0350f7a43085d4396be9e438a2
Robert Gordon <gordonrobertd@gmail.com>
2023-08-12 14:35:19 -0700

    Seeds, now with hashing

--- seeds/userSeeds.js
+++ seeds/userSeeds.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt')
+
 const Models = require("../models");
 
 const data = [
@@ -40,7 +42,7 @@ const data = [
 ];
 
 const seedUsers = async () => {
-  // Loop over data, await is important to resolve the promise (I promise you that the findAll method is a promise)
+  // Loop over data, await is important to resolve the promise (I promise you that the findAll method is a promise) 
   for await (const element of data) {
     // Check if the user exists in the DB already
     const user = await Models.User.findAll({
@@ -63,6 +65,11 @@ const seedUsers = async () => {
     // Check if the data returned has a user or not
     if (user.length === 0) {
       // If no user, add one
+      const rounds = 10 //any more than 10 will take exponentially more CPU power
+      const salt = await bcrypt.genSaltSync(rounds);
+      const originalPassword = element.password
+      const hashedPassword = bcrypt.hashSync(originalPassword, salt);
+      element.password = hashedPassword;
       Models.User.create(element)
         .then((data) => {
           console.log("Added", element);
@@ -73,6 +80,7 @@ const seedUsers = async () => {
         });
     } else {
       console.log("User exists", user[0].email);
+      //Run update
     }
   }
 };
